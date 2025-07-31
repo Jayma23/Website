c4f280f529b5794509d0ad97baabdd58cb605800
James Ma <jamesma@Jamess-MacBook-Pro-2.local>
2025-07-31 09:08:40 -0700

    fix webpack.congig,prod.js

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,4 +1,5 @@
 const { merge } = require('webpack-merge');
+const path = require('path');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
@@ -7,19 +8,20 @@ module.exports = merge(common, {
   mode: 'production',
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: path.resolve(__dirname, 'public/index.html'), // ✅ 修复路径
+      filename: 'index.html',
     }),
     new CopyPlugin({
       patterns: [
-        { from: 'img', to: 'img' },
-        { from: 'css', to: 'css' },
-        { from: 'js/vendor', to: 'js/vendor' },
-        { from: 'icon.svg', to: 'icon.svg' },
-        { from: 'favicon.ico', to: 'favicon.ico' },
-        { from: 'robots.txt', to: 'robots.txt' },
-        { from: 'icon.png', to: 'icon.png' },
-        { from: '404.html', to: '404.html' },
-        { from: 'site.webmanifest', to: 'site.webmanifest' },
+        { from: 'public/img', to: 'img' },
+        { from: 'public/css', to: 'css' },
+        { from: 'public/js/vendor', to: 'js/vendor' },
+        { from: 'public/icon.svg', to: 'icon.svg' },
+        { from: 'public/favicon.ico', to: 'favicon.ico' },
+        { from: 'public/robots.txt', to: 'robots.txt' },
+        { from: 'public/icon.png', to: 'icon.png' },
+        { from: 'public/404.html', to: '404.html' },
+        { from: 'public/site.webmanifest', to: 'site.webmanifest' },
       ],
     }),
   ],
